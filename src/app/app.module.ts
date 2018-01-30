55557bf5f506e16cd57492b7dafcb7da4a1a96a9
M.Sami Mohsin <k122048@nu.edu.pk>
2018-01-31 00:55:29 +0500

    server app angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
