982f252aba4698da02acdbb15cb3ad2a54d5af4d
Sami Mohsin <sami.mohsin@tenpearls.com>
2018-01-31 17:31:06 +0500

    app architecture completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule} from '@angular/common';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeComponent} from './recipes/recipe.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    HeaderComponent,
+    RecipeComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
